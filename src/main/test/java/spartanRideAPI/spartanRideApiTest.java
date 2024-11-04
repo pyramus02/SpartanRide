e8ea3ae6738886fef09985327d9dbbd595cfbf0e
8by4 <aidanquinn147@gmail.com>
2024-11-04 15:42:41 -0500

    Add springboot and maven

---
+++ src/main/test/java/spartanRideAPI/spartanRideApiTest.java
@@ -0,0 +1,13 @@
+package aq340.IA4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ia4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
